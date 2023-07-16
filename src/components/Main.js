3c620c729104c582e0d81564dd0692ffcd8535e8
itsRobertoBorges <robbyborges98@outlook.com>
2023-07-16 09:24:13 -0400

    added custom cursor image

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,7 +18,20 @@ const Main = ({ searchResults }) => {
   }
 
   return (
+    
     <div className="container">
+      <div className="card" style={{ width: '18rem' }}>
+        <img className="card-img-top" src="..." alt="Card image cap" />
+        <div className="card-body bg-primary">
+          <h5 className="card-title text-warning fw-bold">Made by: Roberto Borges</h5>
+          <p className="card-text text-warning italic">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
       <br />
       <br />
       {searchResults.map((result) => (
