684904a7ac5a4ee0b8da7c50d818a0266384df37
itsRobertoBorges <robbyborges98@outlook.com>
2023-07-15 21:05:08 -0400

    added main and navbar components and api works

---
+++ src/components/Main.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+const Main = ({ searchResults }) => {
+  console.log('searchResults:', searchResults);
+
+  if (!searchResults) {
+    return (
+      <div className="text-warning text-center">
+        <h1 className="fw-bold italic">Made by: Roberto Borges</h1>
+      </div>
+    );
+  }
+
+  if (!Array.isArray(searchResults)) {
+    return <div>Invalid search results.</div>;
+  }
+
+  return (
+    <div className="container">
+      <br />
+      <br />
+      {searchResults.map((result) => (
+        <div key={result.defid}>
+          <h1 className='text-warning fw-bold'>{result.word}</h1>
+          <p className="text-warning fw-bold">Definition: {result.definition}</p>
+          <p className="text-warning italic">Example: {result.example}</p>
+          <p className='text-white'>👍 {result.thumbs_up}</p>
+          <p className='text-white'>👎 {result.thumbs_down}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Main from './Main';
+
+const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState(null);
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: 'GET',
+      url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
+      params: { term: searchTerm },
+      headers: {
+        'X-RapidAPI-Key': 'cdf3d91edfmsh0cf7c8cf234009cp1458dfjsn632413f374b4',
+        'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com',
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      const data = response.data.list;
+      setSearchResults(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <nav className="navbar bg-warning bg-gradient">
+        <div className="container-fluid">
+          <a className="navbar-brand text-primary fw-bold fw-italic">Urban Dictionary API Application</a>
+          <form className="d-flex" onSubmit={handleSearch} role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+            <button className="btn btn-outline-success bg-primary text-white" type="button" onClick={handleSearch}>
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      {searchResults && <Main searchResults={searchResults} />}
+    </div>
+  );
+};
+
+export default Navbar;
