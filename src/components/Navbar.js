b5f737e7fa69dc8e150508e47ae37abaa4502265
itsRobertoBorges <robbyborges98@outlook.com>
2023-07-20 10:30:13 -0400

    changed font style, animated card

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar bg-warning bg-gradient shadow-lg fixed-top">
-        <div className="container-fluid">
+        <div className="container">
           <a className="navbar-brand text-primary fw-bold fw-italic">Urban Dictionary API Application</a>
           <form className="d-flex" onSubmit={handleSearch} role="search">
             <input
